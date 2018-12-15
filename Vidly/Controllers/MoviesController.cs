2eaf3d4a42f7b0bcabf03229905a75bea7b7e7d2
Zib <aakhtar76900@gmail.com>
2018-12-15 18:57:23 +0800

    Session ar RMZ Millenia

---
+++ Vidly/Controllers/MoviesController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Vidly.Models;
+using Vidly.ViewModel;
+
+namespace Vidly.Controllers
+{
+    public class MoviesController : Controller
+    {
+        // GET: Movies
+        public ActionResult Random()
+        {
+            var movies = new Movie() { Name = "Shrek" };
+
+            var customers = new List<Customer>()
+            {
+                new Customer {Name="Seb"},
+                new Customer {Name="Kimi"}
+            };
+
+            var viewModel = new RandomMoviewViewModel()
+            {
+                Movie = movies,
+                Customers = customers
+            };
+
+
+            return View(viewModel);
+
+        }
+
+        public ActionResult Edit(int id)
+        {
+            return Content("id=" + id);
+        }
+
+        [Route("movies/released/{year}/{month}")]
+        public ActionResult ByReleaseDate(int year, int month)
+        {
+            return Content(year + "/" + month);
+        }
+
+
+    }
+}
